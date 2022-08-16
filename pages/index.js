fe13c4e7e4d47af0b295d68002cf890407a565d4
LAPTOP-D7FTRIR6\andre <andrew.persaud.dev@gmail.com>
2022-08-16 17:02:00 -0400

    added in a footer

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Image from 'next/image';
 
 import { SearchIcon, MicrophoneIcon } from '@heroicons/react/solid';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -35,6 +36,7 @@ export default function Home() {
         </div>
       </form>
       {/* Footer */}
+      <Footer />
     </div>
   );
 }
